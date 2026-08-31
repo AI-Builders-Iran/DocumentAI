7bb46a3ef89fd04ca3cc881fc49051124fe1edaa
Nastaran yavari <nastaranyavari512@gmail.com>
2026-08-31 15:08:16 +0330

    feat: add contract and common schemas

--- src/schemas/common.py
+++ src/schemas/common.py
@@ -1,10 +1,12 @@
 from enum import Enum
 from pydantic import BaseModel
 
+#-------------support document type in perossesing--------------------
 class DocumentType(str, Enum):
     CONTRACT = "contract"
     INVOICE = "invoice"
     GENERAL = "general"
 
+#-------------schema extracting for other document--------------------
 class GeneralDocument(BaseModel):
     document_type: DocumentType = DocumentType.GENERAL
